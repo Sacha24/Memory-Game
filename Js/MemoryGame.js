5524f5e6e7a8308818a53ddb0a42bf4f42916ee1
Sacha <sacha.auday@hotmail.fr>
2018-11-17 18:40:44 +0200

    levels added

--- Js/MemoryGame.js
+++ Js/MemoryGame.js
@@ -1,13 +1,46 @@
 function newGame() {
-  $("button").css("display", "none");
+  $("#NewGame").css("display", "none");
+  $("#levels").css("display", "block");
+}
+$("#NewGame").on("click", newGame);
+
+function pickLevelEasy() {
   var images = $(".memory-card");
   images.css("display", "block");
+  $("#levels").css("display", "none");
+  src.splice(-8);
+  for (var i = 12; i < 20; i++) {
+    $("#card" + i).css("display", "none");
+  }
 }
-$("button").on("click", newGame);
+$("#easy").on("click", pickLevelEasy);
+
+function pickLevelMedium() {
+  var images = $(".memory-card");
+  images.css("display", "block");
+  $("#levels").css("display", "none");
+  src.splice(-4);
+  for (var i = 16; i < 20; i++) {
+    $("#card" + i).css("display", "none");
+  }
+}
+$("#medium").on("click", pickLevelMedium);
+
+function pickLevelHard() {
+  var images = $(".memory-card");
+  images.css("display", "block");
+  $("#levels").css("display", "none");
+  $(".memory-game").css("width", "840px");
+  $("#wrongGuesses").css("left", "220px");
+}
+$("#hard").on("click", pickLevelHard);
 
 var flippedCard = 0;
+var wrongGuesses = 0;
 var cardsPicked = [];
-function flipCard1(event) {
+var endGame = [];
+
+var flipCard = function(event) {
   var cardTarget = event.currentTarget;
   cardsPicked.push(cardTarget);
   if ((cardTarget.style.transform = "rotateY(180deg)")) {
@@ -19,22 +52,33 @@ function flipCard1(event) {
     var match = checkIfMatch();
 
     if (!match) {
+      wrongGuesses++;
       setTimeout(function() {
         $(".memory-card.unmatch").css("pointer-events", "auto");
         $(".memory-card.unmatch").css({ transform: "rotateY(" + 0 + "deg)" });
       }, 2000);
     } else {
+      endGame.push(cardTarget);
       $(".memory-card.unmatch").css("pointer-events", "auto");
-      cardsPicked[0].classList.replace("unmatch","match")
-      cardsPicked[1].classList.replace("unmatch","match")
+      cardsPicked[0].classList.replace("unmatch", "match");
+      cardsPicked[1].classList.replace("unmatch", "match");
       cardsPicked.splice(0, 2);
-      /* $(".memory-card.match").css({ transform: "rotateY(" + 180 + "deg)" }); */
     }
     flippedCard = 0;
+    $("#wrongGuesses").html("Wrong guesses : " + wrongGuesses);
   }
-}
+  if (endGame.length === src.length / 2) {
+    setTimeout(function() {
+      var images = $(".memory-card");
+      images.fadeTo(1000, 0);
+    }, 3000);
+    setTimeout(function() {
+      $("#restart").css("display", "block");
+    }, 5000);
+  }
+};
 var card = $(".memory-card");
-card.on("click", flipCard1);
+card.on("click", flipCard);
 
 function checkIfMatch() {
   if (
@@ -48,19 +92,33 @@ function checkIfMatch() {
   }
 }
 
+function resetGame() {
+  location.reload();
+}
+
+$("#restart").on("click", resetGame);
+
 var src = [
   "images/Captain-America.jpg",
-  "images/Iron Man.jpg",
-  "images/Hulk.png",
-  "images/thor.jpeg",
-  "images/Barton.jpg",
-  "images/black widow.jpg",
   "images/Captain-America.jpg",
   "images/Iron Man.jpg",
+  "images/Iron Man.jpg",
+  "images/Hulk.png",
   "images/Hulk.png",
   "images/thor.jpeg",
+  "images/thor.jpeg",
   "images/Barton.jpg",
-  "images/black widow.jpg"
+  "images/Barton.jpg",
+  "images/black widow.jpg",
+  "images/black widow.jpg",
+  "images/dr strange.jpg",
+  "images/dr strange.jpg",
+  "images/black panther.jpg",
+  "images/black panther.jpg",
+  "images/quill.jpg",
+  "images/quill.jpg",
+  "images/antman.jpg",
+  "images/antman.jpg"
 ];
 
 function shuffle() {
@@ -71,4 +129,4 @@ function shuffle() {
     $("#img" + i).attr("src", shuffleArray[i]);
   }
 }
-document.getElementById("button").addEventListener("click", shuffle);
+$("li").on("click", shuffle);
